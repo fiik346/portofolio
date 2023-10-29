8dbfe5240bcda77a7b6ad9468f9491b21f591778
Taufik Nurhidayat <fiik346@gmail.com>
2023-10-29 10:20:29 +0700

    chore: remove preact

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,11 +2,9 @@ import { defineConfig } from 'astro/config';
 import nodejs from "@astrojs/nodejs";
 import tailwind from "@astrojs/tailwind";
 
-import preact from "@astrojs/preact";
-
 // https://astro.build/config
 export default defineConfig({
   output: 'server',
   adapter: nodejs(),
-  integrations: [tailwind(), preact()]
+  integrations: [tailwind()]
 });
