261a52ddc214b18327805a0d9ee723c09024ccbd
Taufik Nurhidayat <fiik346@gmail.com>
2023-10-29 10:17:32 +0700

    add: fetch server side

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,12 @@
 import { defineConfig } from 'astro/config';
+import nodejs from "@astrojs/nodejs";
 import tailwind from "@astrojs/tailwind";
 
 import preact from "@astrojs/preact";
 
 // https://astro.build/config
 export default defineConfig({
+  output: 'server',
+  adapter: nodejs(),
   integrations: [tailwind(), preact()]
 });
