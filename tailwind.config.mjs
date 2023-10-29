172c02a546153ee5917605c025dfa8780bc72402
Taufik Nurhidayat <fiik346@gmail.com>
2023-10-29 11:52:04 +0700

    add: inter fonts

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -4,6 +4,9 @@ export default {
   content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
   theme: {
     extend: {
+      fontFamily: {
+        sans: ["Inter", "ui-sans-serif", "system-ui", "-apple-system", "BlinkMacSystemFont", "Segoe UI", "Roboto", "Helvetica Neue", "Arial", "Noto Sans", "sans-serif", "Apple Color Emoji", "Segoe UI Emoji", "Segoe UI Symbol", "Noto Color Emoji"]
+      },
       colors: {
         primary: colors.blue,
       },
