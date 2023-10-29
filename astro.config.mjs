ddaa381a4b9c548812126ce6cf8e05d4f08c7ffe
Taufik Nurhidayat <fiik346@gmail.com>
2023-10-29 10:48:09 +0700

    chore: using pnpm again

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,10 +1,16 @@
 import { defineConfig } from 'astro/config';
-import nodejs from "@astrojs/nodejs";
+import node from "@astrojs/node"
 import tailwind from "@astrojs/tailwind";
 
 // https://astro.build/config
 export default defineConfig({
   output: 'server',
-  adapter: nodejs(),
+  adapter: node({
+    mode: 'standalone'
+  }),
+  image: {
+    domains: ['res.cloudinary.com'],
+    remotePatterns: [{ protocol: "https" }],
+  },
   integrations: [tailwind()]
 });
