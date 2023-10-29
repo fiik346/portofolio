010348a7080433ce157af327cf3f8314a961b372
Taufik Nurhidayat <fiik346@gmail.com>
2023-10-29 11:32:34 +0700

    perfomance update

---
+++ src/style/button.ts
@@ -0,0 +1,6 @@
+const button_base = "text-sm px-4 py-3 duration-300 inline-flex rounded"
+export const button_style = {
+  primary: `${button_base} bg-primary-500 text-white hover:shadow-lg hover:shadow-primary-600/50 hover:bg-primary-600 ring-offset-2 focus:ring-2`,
+  primary_outline: `${button_base} text-primary-500 border border-primary-500 hover:bg-primary-500 hover:text-white ring-offset-2 focus:ring-2`,
+  ghost: `${button_base} duration-300 inline-flex items-center px-4 py-3 rounded hover:text-slate-500 hover:bg-slate-100`
+}
